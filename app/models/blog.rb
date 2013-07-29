27b82034666a5faa6bed88427609347be2ceb487
Alexander Petrov <alexpetrov.rb@gmail.com>
2013-07-30 00:41:25 +0400

    add new_post method to Blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,7 @@
 
 class Blog
   attr_reader :entries
+  attr_writer :post_source
 
   def initialize
     @entries = []
@@ -13,5 +14,17 @@ class Blog
 
   def subtitle
     "The trusted source for drying paint news & opinion"
+  end
+
+  def new_post
+    post_source.call.tap do |p|
+      p.blog = self
+    end
+  end
+
+  private
+
+  def post_source
+    @post_source ||= Post.public_method(:new)
   end
 end
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,10 +1,26 @@
 # spec/models/blog_spec.rb
 require 'minitest/autorun'
+require 'ostruct'
 require_relative '../../app/models/blog'
 
 describe Blog do
   before do
     @it = Blog.new
+  end
+
+  describe "#new_post" do
+    before do
+      @new_post = OpenStruct.new
+      @it.post_source = ->{ @new_post }
+    end
+
+    it "returns a new post" do
+      @it.new_post.must_equal @new_post
+    end
+
+    it "sets the post's blog reference to itself" do
+      @it.new_post.blog.must_equal(@it)
+    end
   end
 
   it "has no entries" do
