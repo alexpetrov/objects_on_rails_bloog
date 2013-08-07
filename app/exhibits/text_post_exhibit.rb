f892c262d4b98251cd5cb87bb0bf61759c501551
Alexander Petrov <alexpetrov.rb@gmail.com>
2013-08-07 21:45:55 +0400

    refs issue #1: improve debug message, to indicate exactly this issue

--- app/exhibits/text_post_exhibit.rb
+++ app/exhibits/text_post_exhibit.rb
@@ -9,7 +9,7 @@ class TextPostExhibit < Exhibit
   end
 
   def self.applicable_to?(object)
-    Rails.logger.debug("Object class = #{object.class}; object.kind_of?(Post) = #{object.kind_of?(Post)}; object.is_a?(Post) = #{object.is_a?(Post)}")
+    Rails.logger.debug("Issue #1: Heisenbug debug info: Object class = #{object.class}; object.kind_of?(Post) = #{object.kind_of?(Post)}; object.is_a?(Post) = #{object.is_a?(Post)}")
     object.kind_of?(Post) &&
     (!(object.picture?))
   end
