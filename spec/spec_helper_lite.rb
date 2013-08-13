3f71dcb5fe16efd86b7b109c841a91e637b11587
Alexander Petrov <alexpetrov.rb@gmail.com>
2013-08-13 23:44:58 +0400

    Remove unnecessary include RR into test framework base class

--- spec/spec_helper_lite.rb
+++ spec/spec_helper_lite.rb
@@ -7,11 +7,6 @@ require 'active_support/deprecation'
 require 'activerecord-nulldb-adapter'
 require 'minitest/autorun'
 
-
-class MiniTest::Unit::TestCase
-  include RR::Adapters::MiniTest
-end
-
 def stub_module(full_name)
   full_name.to_s.split(/::/).inject(Object) do |context, name|
     begin
