778476af82df7266ad9d408761ea505d17dc6141
Alexander Petrov <alexpetrov.rb@gmail.com>
2013-08-14 00:02:44 +0400

    fix bug with undefined method validates for ActiveRecord::Base:Class by removing require nulldb gem

--- spec/spec_helper_full.rb
+++ spec/spec_helper_full.rb
@@ -1,7 +1,15 @@
 # spec/spec_helper_full.rb
-require_relative 'spec_helper_lite'
+#require_relative 'spec_helper_lite'
+require 'rr'
+require 'date'
+require 'ostruct'
+require 'active_support'
+require 'active_support/deprecation'
+
 require_relative '../config/environment.rb'
 require 'database_cleaner'
+require 'minitest/autorun'
+
 
 module SpecHelpers
   def setup_database
